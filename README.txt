ddae92b70d2edce9ba65ed37042232eeaf2a990f
uhuhlazy <ianochka935@gmail.com>
2025-11-15 18:54:38 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+Deploy on Render:
+1. Add env BOT_TOKEN
+2. worker type
+3. Deploy
---
+++ bot.py
@@ -0,0 +1,50 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import os
+
+SPAM_WORDS = [
+    "подарок","подарочки","подарок из профиля","подарочек из профиля",
+    "приз","призы","выиграли","выигрыш","розыгрыш",
+    "акция","акции","бонус","бонусы",
+    "скидка","скидки","скидк","дешево","дёшево",
+    "инвести","инвест","инвестировать",
+    "крипт","крипта","криптовалют",
+    "nft","куплю nft","купить nft","продам nft",
+    "хочу nft","твое nft","твой nft",
+    "куплю кот","куплю кота","купить кота","купить кот",
+    "кот куплю","котик куплю","котика куплю","котёнка куплю",
+    "куплю твоего кота",
+    "куплю","купить","хочу купить","готов купить",
+    "интересует покупка","предложение покупки",
+    "предлагаю купить","куплю у тебя",
+    "куплю твой подарок","купить подарок",
+    "купить твой подарок","куплю подарок из профиля",
+    "куплю твой подарок из профиля",
+    "срочно","очень срочно",
+    "перейди","перейди по ссылке","ссылка","по ссылке"
+]
+
+REPLY_TEXT="Ваше сообщение было автоматически помечено как спам. Дальнейшая переписка по данному вопросу не ведется."
+
+async def check_spam(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.effective_message
+    if not msg or not msg.text:
+        return
+    text = msg.text.lower()
+    for spam in SPAM_WORDS:
+        if spam in text:
+            await msg.reply_text(REPLY_TEXT)
+            return
+
+def main():
+    TOKEN=os.environ.get("BOT_TOKEN")
+    if not TOKEN:
+        print("ERROR: BOT_TOKEN not set")
+        return
+    app=ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT, check_spam))
+    print("Bot is running on Render...")
+    app.run_polling()
+
+if __name__=="__main__":
+    main()
